812dfb40cd8e356ad3159879fa9fb76b3385bdd7
Rahul Sunil <rahul.sunil@accubits.com>
2023-04-04 19:06:09 +0530

    Home Routes

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,5 +2,13 @@
 
 <?= $this->section('content'); ?>
 <h1>Welcome to CarShala</h1>
-<p>Thank you for choosing us for your car rental needs.</p>
+<ul>
+    <li><a href="<?php echo base_url('login'); ?>">Login</a></li>
+    <li><a href="<?php echo base_url('logout'); ?>">Logout</a></li>
+    <li><a href="<?php echo base_url('auth/register/agency'); ?>">Register as Agency</a></li>
+    <li><a href="<?php echo base_url('auth/register/customer'); ?>">Register as Customer</a></li>
+    <li><a href="<?php echo base_url('cars/add'); ?>">Add Car</a></li>
+    <li><a href="<?php echo base_url('cars/booked-cars'); ?>">View Booked Cars</a></li>
+    <li><a href="<?php echo base_url('customers/bookings'); ?>">View Bookings</a></li>
+</ul>
 <?= $this->endSection(); ?>
